5c7da274e4fb8349e1af7b6d1da321de00b3b4ed
Julius Frick <julius.frick@finn.com>
2026-01-19 14:25:14 +0100

    Fix Sparkle update feed URL: migrate from DailyDigest to DailyBriefing
    - Update default feed URL in UpdateService to use DailyBriefing path
    - Update all build scripts and release workflow to use new path
    - Add automatic migration logic in release workflow to migrate old appcast.xml
    - Update README with correct appcast URL

--- DailyBriefing/Sources/Core/Services/UpdateService.swift
+++ DailyBriefing/Sources/Core/Services/UpdateService.swift
@@ -12,7 +12,7 @@ final class UpdateService: NSObject, ObservableObject {
     // MARK: - Constants
 
     /// Default appcast URL used when Info.plist doesn't provide one
-    private static let defaultFeedURL = "https://juliusfrick.github.io/DailyDigest/appcast.xml"
+    private static let defaultFeedURL = "https://juliusfrick.github.io/DailyBriefing/appcast.xml"
 
     // MARK: - Published Properties
 
